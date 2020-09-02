87f42d1f5cd486916fd6e6f6d541f9dceb377717
Yoel <s73017@beuth-hochschule.de>
2020-09-02 18:59:02 +0200

    Switched out public and private properties for consistency

--- RayTracer/material/texture/Constant.h
+++ RayTracer/material/texture/Constant.h
@@ -5,12 +5,11 @@
 
 namespace material {
 class Constant : public util::Sampler {
-   private:
-	const util::Vec3 albedo;
-
    public:
 	Constant(const util::Vec3& albedo);
-
 	util::Vec3 color(float x, float y) const override;
+
+   private:
+	const util::Vec3 albedo;
 };
 }  // namespace material
