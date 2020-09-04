56dcba7f56f903909126ddea157ea96b7cc8603c
Yoel <s73017@beuth-hochschule.de>
2020-09-04 14:38:33 +0200

    Put the definitions for the Background-methods in the right namespace

--- RayTracer/shape/Background.cpp
+++ RayTracer/shape/Background.cpp
@@ -2,14 +2,14 @@
 
 #include <limits>
 
-shapes::Background::Background(
-    const std::shared_ptr<material::Material>& material)
+namespace shapes {
+Background::Background(const std::shared_ptr<material::Material>& material)
     : material(material) {
 }
 
-std::shared_ptr<cam::Hit> shapes::Background::intersect(
-    const cam::Ray& r) const {
+std::shared_ptr<cam::Hit> Background::intersect(const cam::Ray& r) const {
 	return std::make_shared<cam::Hit>(
 	    cam::Hit(r(std::numeric_limits<float>::infinity()), util::Vec3(0, 0, 0),
 	             std::numeric_limits<float>::infinity(), material));
 }
+}  // namespace shapes
