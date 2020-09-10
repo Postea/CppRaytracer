d7f4533e3304262578112e4496527794621ff9b6
Yoel <s73017@beuth-hochschule.de>
2020-09-10 22:06:29 +0200

    Made the material of the hit visible again

--- RayTracer/camera/Hit.h
+++ RayTracer/camera/Hit.h
@@ -18,10 +18,11 @@ class Hit {
 	float scalar() const;
 	// Attributes
 	// TODO TexelPos
+	std::shared_ptr<material::Material> material;
+
    private:
 	util::Vec3 hit, n;
 	float t;
-	std::shared_ptr<material::Material> material;
 };
 
 }  // namespace cam
