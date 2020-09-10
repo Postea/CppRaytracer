779ef15161f459af8a6800d0f21c452da73cc926
Yoel <s73017@beuth-hochschule.de>
2020-09-10 17:20:16 +0200

    Changed const members to private and declared and defined getter

--- RayTracer/camera/Hit.h
+++ RayTracer/camera/Hit.h
@@ -8,15 +8,20 @@ class Hit {
    public:
 	Hit(const util::Vec3& hit, const util::Vec3& n, float t,
 	    const std::shared_ptr<material::Material>& material);
+
 	Ray scattered_ray(const Ray& inc_ray) const;
 	util::Vec3 albedo() const;    // TODO TexelPos
 	util::Vec3 emission() const;  // TODO TexelPos
 	bool scatter() const;
+	util::Vec3 hitpoint() const;
+	util::Vec3 normal() const;
+	float scalar() const;
 	// Attributes
 	// TODO TexelPos
-	const util::Vec3 hit, n;
-	const float t;
-	const std::shared_ptr<material::Material> material;
+   private:
+	util::Vec3 hit, n;
+	float t;
+	std::shared_ptr<material::Material> material;
 };
 
 }  // namespace cam
