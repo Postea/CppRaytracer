70efd9cbea6719a08ef8428fe5b9e4a71e721d46
Yoel <s73017@beuth-hochschule.de>
2020-10-06 17:27:32 +0200

    Sphere is now a light which its defined functions

--- RayTracer/shape/Sphere.h
+++ RayTracer/shape/Sphere.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "Light.h"
 #include "Shape.h"
 
 namespace shapes {
-class Sphere : public Shape {
+class Sphere : public Shape, public Light {
    public:
 	Sphere(float radius, const std::shared_ptr<material::Material>& material);
 	std::optional<cam::Hit> intersect(const cam::Ray& r) const override;
 	util::AxisAlignedBoundingBox bounds() const override;
+	cam::Hit sampleLight() const override;
 
    private:
 	std::shared_ptr<material::Material> material;
