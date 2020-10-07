f0f24511c56682794f467b18989cfe5700ecbaa6
Yoel <s73017@beuth-hochschule.de>
2020-10-07 21:22:59 +0200

    Refactored the hit class to be a surfacepoint

--- RayTracer/camera/Hit.cpp
+++ RayTracer/camera/Hit.cpp
@@ -5,27 +5,9 @@
 namespace cam {
 Hit::Hit(const util::Vec3& hit, const util::Vec3& n, float t,
          const std::shared_ptr<material::Material>& material)
-    : hit(hit), n(n), t(t), material(material) {
+    : util::SurfacePoint(hit, n, material), t(t) {
 }
-Ray Hit::scattered_ray(const Ray& inc_ray) const {
-	return Ray(hit, material->scattered_d(inc_ray.d, n),
-	           std::numeric_limits<float>::epsilon(), inc_ray.tmax, true);
-}
-util::Vec3 Hit::albedo() const {
-	return material->albedo(0, 0);
-}
-util::Vec3 Hit::emission() const {
-	return material->emission(0, 0);
-}
-bool Hit::scatter() const {
-	return material->scatter();
-}
-util::Vec3 Hit::hitpoint() const {
-	return hit;
-}
-util::Vec3 Hit::normal() const {
-	return n;
-}
+
 float Hit::scalar() const {
 	return t;
 }
--- RayTracer/camera/Hit.h
+++ RayTracer/camera/Hit.h
@@ -1,27 +1,18 @@
 #pragma once
 
 #include "../material/Material.h"
+#include "../tools/SurfacePoint.h"
 #include "Ray.h"
 
 namespace cam {
-class Hit {
+class Hit : public util::SurfacePoint {
    public:
 	Hit(const util::Vec3& hit, const util::Vec3& n, float t,
 	    const std::shared_ptr<material::Material>& material);
 
-	Ray scattered_ray(const Ray& inc_ray) const;
-	util::Vec3 albedo() const;    // TODO TexelPos
-	util::Vec3 emission() const;  // TODO TexelPos
-	bool scatter() const;
-	util::Vec3 hitpoint() const;
-	util::Vec3 normal() const;
 	float scalar() const;
-	// Attributes
-	// TODO TexelPos
-	std::shared_ptr<material::Material> material;
 
    private:
-	util::Vec3 hit, n;
 	float t;
 };
 
