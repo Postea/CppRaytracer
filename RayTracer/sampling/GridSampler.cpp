cfe6aad1f1e8e8f11c484c2ab3885f68ebb5db54
Yoel <s73017@beuth-hochschule.de>
2020-10-07 21:23:32 +0200

    Changes from doubles to floats in 4 instances

--- RayTracer/sampling/GridSampler.cpp
+++ RayTracer/sampling/GridSampler.cpp
@@ -8,8 +8,8 @@ Vec3 GridSampler::color(float x, float y) const {
 	Vec3 color(0, 0, 0);
 	for (int xi = 0; xi < n; xi++) {
 		for (int yi = 0; yi < n; yi++) {
-			double xs = x + (xi + 0.5) / n;
-			double ys = y + (yi + 0.5) / n;
+			float xs = x + (xi + 0.5) / n;
+			float ys = y + (yi + 0.5) / n;
 			color = color + sampler->color(xs, ys);
 		}
 	}
