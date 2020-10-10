562839f77235aa129291879c0b56b7d09a2c9354
Yoel <s73017@beuth-hochschule.de>
2020-10-10 16:40:57 +0200

    Refactor to use Random class

--- RayTracer/sampling/StratifiedSampler.cpp
+++ RayTracer/sampling/StratifiedSampler.cpp
@@ -9,8 +9,8 @@ Vec3 StratifiedSampler::color(float x, float y) const {
 	Vec3 color(0, 0, 0);
 	for (int xi = 0; xi < n; xi++) {
 		for (int yi = 0; yi < n; yi++) {
-			float rx = dis(gen);
-			float ry = dis(gen);
+			float rx = dis0to1(gen);
+			float ry = dis0to1(gen);
 			float xs = x + (xi + rx) / n;
 			float ys = y + (yi + ry) / n;
 			color = color + sampler->color(xs, ys);
