5b5ee8bc0d8bd47717ab48ea7a5652101cb33a6e
Yoel <s73017@beuth-hochschule.de>
2020-10-16 16:07:09 +0200

    Light now has a function that determines the amount of light emitted by the Light in direction d

--- RayTracer/shape/Light.h
+++ RayTracer/shape/Light.h
@@ -5,5 +5,7 @@ namespace shapes {
 class Light {
    public:
 	virtual util::SurfacePoint sampleLight() const = 0;
+	virtual util::Vec3 calculateLightEmission(const util::SurfacePoint& p,
+	                                          const util::Vec3& d) const = 0;
 };
 }  // namespace shapes
