ea29da3880889389cae5d9742500c6783ef0f210
Yoel <s73017@beuth-hochschule.de>
2020-10-17 17:05:38 +0200

    Changed scatter-func to depend on incoming dir and n, also added a calcLightMultiplier-func

--- RayTracer/material/Material.h
+++ RayTracer/material/Material.h
@@ -13,6 +13,9 @@ class Material {
 	virtual util::Vec3 emission(float texel_x, float texel_y) const = 0;
 	virtual util::Vec3 scattered_d(const util::Vec3& d,
 	                               const util::Vec3& n) const = 0;
-	virtual bool scatter() const = 0;
+	virtual bool scatter(const util::Vec3& d, const util::Vec3& n) const = 0;
+	virtual float calculateLightMultiplier(const util::Vec3& d_in,
+	                                       const util::Vec3& d_out,
+	                                       const util::Vec3& n) const = 0;
 };
 }  // namespace material
