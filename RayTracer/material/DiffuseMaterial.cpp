90d47f0b950ac7b6fd7a0ce28cd5c625d324ab9a
Yoel <s73017@beuth-hochschule.de>
2021-03-29 17:45:41 +0200

    Diffuse Refactor

--- RayTracer/material/DiffuseMaterial.cpp
+++ RayTracer/material/DiffuseMaterial.cpp
@@ -12,24 +12,38 @@ namespace material {
 DiffuseMaterial::DiffuseMaterial(
     const std::shared_ptr<util::Sampler>& albedo_texture,
     const std::shared_ptr<util::Sampler>& emission_texture)
-    : albedo_texture(albedo_texture), emission_texture(emission_texture) {
+    : albedo_texture(albedo_texture), emission_profile({emission_texture}) {
+}
+DiffuseMaterial::DiffuseMaterial(
+    const std::shared_ptr<util::Sampler>& albedo_texture,
+    const std::shared_ptr<util::Sampler>& emission_texture,
+    const util::Image& distribution)
+    : albedo_texture(albedo_texture),
+      emission_profile({emission_texture, distribution}) {
 }
 DiffuseMaterial::DiffuseMaterial(
     const std::shared_ptr<util::Sampler>& albedo_texture)
     : albedo_texture(albedo_texture),
-      emission_texture(
-          std::make_shared<Constant>(Constant(util::Vec3(0, 0, 0)))) {
+      emission_profile(
+          {std::make_shared<Constant>(Constant(util::Vec3(0, 0, 0)))}) {
 }
 DiffuseMaterial::DiffuseMaterial(const util::Vec3& color)
     : albedo_texture(std::make_shared<Constant>(Constant(color))),
-      emission_texture(
-          std::make_shared<Constant>(Constant(util::Vec3(0, 0, 0)))) {
+      emission_profile(
+          {std::make_shared<Constant>(Constant(util::Vec3(0, 0, 0)))}) {
 }
 util::Vec3 DiffuseMaterial::albedo(const std::pair<float, float>& uv) const {
 	return albedo_texture->color(uv.first, uv.second);
 }
 util::Vec3 DiffuseMaterial::emission(const std::pair<float, float>& uv) const {
-	return emission_texture->color(uv.first, uv.second);
+	return emission_profile.color(uv.first, uv.second);
+}
+std::pair<float, float> DiffuseMaterial::sampleEmissionProfile() const {
+	return emission_profile.sample();
+}
+
+std::optional<float> DiffuseMaterial::emission_pdf(float u, float v) const {
+	return emission_profile.pdf(u, v);
 }
 util::Vec3 DiffuseMaterial::scattered_d(const util::Vec3& d,
                                         const util::Vec3& n) const {
--- RayTracer/material/DiffuseMaterial.h
+++ RayTracer/material/DiffuseMaterial.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../tools/EmissionProfile.h"
 #include "Material.h"
 
 namespace material {
@@ -7,11 +8,15 @@ class DiffuseMaterial : public Material {
    public:
 	DiffuseMaterial(const std::shared_ptr<util::Sampler>& albedo_texture,
 	                const std::shared_ptr<util::Sampler>& emission_texture);
+	DiffuseMaterial(const std::shared_ptr<util::Sampler>& albedo_texture,
+	                const std::shared_ptr<util::Sampler>& emission_texture,
+	                const util::Image& distribution);
 	DiffuseMaterial(const std::shared_ptr<util::Sampler>& albedo);
 	DiffuseMaterial(const util::Vec3& color);
 
 	util::Vec3 albedo(const std::pair<float, float>& uv) const override;
 	util::Vec3 emission(const std::pair<float, float>& uv) const override;
+	std::pair<float, float> sampleEmissionProfile() const override;
 	util::Vec3 scattered_d(const util::Vec3& d,
 	                       const util::Vec3& n) const override;
 	bool scatter(const util::Vec3& d, const util::Vec3& n) const override;
@@ -19,9 +24,10 @@ class DiffuseMaterial : public Material {
 	float calculateLightMultiplier(const util::Vec3& d_in,
 	                               const util::Vec3& d_out,
 	                               const util::Vec3& n) const override;
+	std::optional<float> emission_pdf(float u, float v) const override;
 
    private:
 	std::shared_ptr<util::Sampler> albedo_texture;
-	std::shared_ptr<util::Sampler> emission_texture;
+	util::EmissionProfile emission_profile;
 };
 }  // namespace material
