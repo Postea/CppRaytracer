46637d06f292e1a86b5c22064b714270ba25b2a5
Yoel <s73017@beuth-hochschule.de>
2021-05-14 16:06:16 +0200

    Materials now have a function: brdf_pdf

--- RayTracer/material/Material.h
+++ RayTracer/material/Material.h
@@ -19,6 +19,8 @@ class Material {
 	virtual float calculateLightMultiplier(const util::Vec3& d_in,
 	                                       const util::Vec3& d_out,
 	                                       const util::Vec3& n) const = 0;
+	virtual float brdf_pdf(const util::Vec3& d_out,
+	                       const util::Vec3& n) const = 0;
 	virtual std::optional<float> emission_pdf(float u, float v) const = 0;
 };
 util::Vec3 reflection(const util::Vec3& d, const util::Vec3& n);
