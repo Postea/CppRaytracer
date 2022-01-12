959ebd5a757c3ceb30dc058ece255c0bed0ff6fe
Yoel <s73017@beuth-hochschule.de>
2022-01-12 13:05:21 +0100

    Ray added << to header, cleanup

--- RayTracer/camera/Ray.h
+++ RayTracer/camera/Ray.h
@@ -4,14 +4,12 @@
 namespace cam {
 class Ray {
    public:
-	// Constructor
 	Ray(const util::Vec3& x0, const util::Vec3& d, float tmin, float tmax,
 	    bool normalize);
-	// Operator
-	friend std::ostream& operator<<(std::ostream& os, const cam::Ray& rhs);
+
 	util::Vec3 operator()(float t) const;
-	// Methods
 	bool in_range(float t) const;
+	friend std::ostream& operator<<(std::ostream& os, const cam::Ray& rhs);
 
 	const util::Vec3 x0;
 	util::Vec3 d;
