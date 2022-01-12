69706300e33d4431a2985a6b35c4568f5fad898b
Yoel <s73017@beuth-hochschule.de>
2022-01-12 13:04:17 +0100

    CamObs cleanup

--- RayTracer/camera/CamObs.cpp
+++ RayTracer/camera/CamObs.cpp
@@ -3,11 +3,12 @@
 #include <limits>
 
 namespace cam {
-cam::CamObs::CamObs(const util::Mat4& mat, float theta, int width, int height)
+CamObs::CamObs(const util::Mat4& mat, float theta, int width, int height)
     : mat(mat), theta(theta), width(width), height(height) {
 }
 
-Ray cam::CamObs::create(float x, float y) const {
+// Create a new ray shooting from the camera
+Ray CamObs::create(float x, float y) const {
 	util::Vec3 d(x - width / 2, height / 2 - y,
 	             -(width / 2) / (tan(theta / 2)));
 
--- RayTracer/camera/CamObs.h
+++ RayTracer/camera/CamObs.h
@@ -6,11 +6,10 @@
 namespace cam {
 class CamObs {
    public:
-	// Constructor
 	CamObs(const util::Mat4& mat, float theta, int width, int height);
-	// Methods
+
 	Ray create(float x, float y) const;
-	// Attributes
+
 	const int width, height;
 
    private:
