64659565750fe6b6224e57dd6208219de1e42d31
Yoel <s73017@beuth-hochschule.de>
2022-04-03 14:30:49 +0200

    Sped up scene building

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -37,21 +37,62 @@ using namespace shapes;
 using namespace std;
 
 size_t threadpool_size = 4;
-/*
-size_t sample_n = 1;
-size_t sample_l = 1;
-size_t max_depth = 8;
+auto s = {1};
+auto l = {0};
+auto d = {8};
 
-auto camera_key = "upperAngle";
-auto sky_key = "StaryNight";
-
-auto camera = config::cameras[camera_key];
-auto sky = config::skies[sky_key];
-*/
 int main() {
-	auto s = {1};
-	auto l = {1};
-	auto d = {8};
+	cout << "Start building scene" << endl;
+	Mat4 ident;
+	Group group(ident);
+	auto checkered_board = make_shared<RectanglePlane>(
+	    30.0f, 30.0f, false,
+	    make_shared<DiffuseMaterial>(
+	        make_shared<Checkerboard>(8, Vec3(0.8), Vec3(0.4))));
+	// floor
+	group.add(ShapeSingleGroup(translate(Vec3(0, 0, 0)), checkered_board));
+	// ceiling
+	group.add(
+	    ShapeSingleGroup(rotate(Vec3(1, 0, 0), 180) * translate(Vec3(0, 10, 0)),
+	                     checkered_board));
+	// back wall
+	group.add(
+	    ShapeSingleGroup(rotate(Vec3(1, 0, 0), 90) * translate(Vec3(0, 8, -5)),
+	                     checkered_board));
+	// front wall
+	group.add(
+	    ShapeSingleGroup(rotate(Vec3(1, 0, 0), -90) * translate(Vec3(0, 8, 8)),
+	                     checkered_board));
+	// left wall
+	group.add(
+	    ShapeSingleGroup(rotate(Vec3(1, 0, 0), 90) * rotate(Vec3(0, 1, 0), 90) *
+	                         translate(Vec3(-8, 8, 0)),
+	                     checkered_board));
+	// right wall
+	group.add(ShapeSingleGroup(rotate(Vec3(1, 0, 0), 90) *
+	                               rotate(Vec3(0, 1, 0), -90) *
+	                               translate(Vec3(8, 8, 0)),
+	                           checkered_board));
+	// cube
+	auto cube = make_shared<TriangleMesh>(
+	    std::ifstream("Cube.obj"), make_shared<DiffuseMaterial>(Vec3(1, 0, 0)));
+	group.add(ShapeSingleGroup(translate(Vec3(0, 1, -3)), cube));
+
+	// light
+	auto rect_light = make_shared<RectanglePlane>(
+	    1.0f, 1.0f, false, make_shared<BackgroundMaterial>(Vec3(1)));
+
+	auto light_transform = rotate(Vec3(1, 0, 0), 90) *
+	                       rotate(Vec3(0, 1, 0), -90) *
+	                       translate(Vec3(7.99, 1.5, 0));
+
+	group.add(ShapeSingleGroup(light_transform, rect_light));
+	auto skysphere = make_shared<SkySphere>(make_shared<Constant>(Vec3(50)));
+	group.add(skysphere);
+
+	std::vector<std::shared_ptr<Light> > lights = {
+	    make_shared<LightSingleGroup>(light_transform, rect_light)};
+	cout << "Start rendering" << endl;
 
 	for (const auto& [camera_key, camera] : config::cameras) {
 		CamObs obs(camera, M_PI / 2, 600, 360);
@@ -59,67 +100,6 @@ int main() {
 		for (const auto& sample_n : s) {
 			for (const auto& sample_l : l) {
 				for (const auto& max_depth : d) {
-					cout << "Start" << endl;
-					// Image img = Image (100, 100);
-
-					Mat4 ident;
-					CamObs obs(camera, M_PI / 2, 600, 360);
-
-					Group group(ident);
-					auto checkered_board = make_shared<RectanglePlane>(
-					    30.0f, 30.0f, false,
-					    make_shared<DiffuseMaterial>(make_shared<Checkerboard>(
-					        8, Vec3(0.8), Vec3(0.4))));
-					// floor
-					group.add(ShapeSingleGroup(translate(Vec3(0, 0, 0)),
-					                           checkered_board));
-					// ceiling
-					group.add(ShapeSingleGroup(
-					    rotate(Vec3(1, 0, 0), 180) * translate(Vec3(0, 10, 0)),
-					    checkered_board));
-					// back wall
-					group.add(ShapeSingleGroup(
-					    rotate(Vec3(1, 0, 0), 90) * translate(Vec3(0, 8, -5)),
-					    checkered_board));
-					// front wall
-					group.add(ShapeSingleGroup(
-					    rotate(Vec3(1, 0, 0), -90) * translate(Vec3(0, 8, 8)),
-					    checkered_board));
-					// left wall
-					group.add(ShapeSingleGroup(rotate(Vec3(1, 0, 0), 90) *
-					                               rotate(Vec3(0, 1, 0), 90) *
-					                               translate(Vec3(-8, 8, 0)),
-					                           checkered_board));
-					// right wall
-					group.add(ShapeSingleGroup(rotate(Vec3(1, 0, 0), 90) *
-					                               rotate(Vec3(0, 1, 0), -90) *
-					                               translate(Vec3(8, 8, 0)),
-					                           checkered_board));
-					// cube
-					auto cube = make_shared<TriangleMesh>(
-					    std::ifstream("Cube.obj"),
-					    make_shared<DiffuseMaterial>(Vec3(1, 0, 0)));
-					group.add(
-					    ShapeSingleGroup(translate(Vec3(0, 1, -3)), cube));
-
-					// light
-					auto rect_light = make_shared<RectanglePlane>(
-					    1.0f, 1.0f, false,
-					    make_shared<BackgroundMaterial>(Vec3(1)));
-
-					auto light_transform = rotate(Vec3(1, 0, 0), 90) *
-					                       rotate(Vec3(0, 1, 0), -90) *
-					                       translate(Vec3(7.99, 1.5, 0));
-
-					group.add(ShapeSingleGroup(light_transform, rect_light));
-
-					std::vector<std::shared_ptr<Light> > lights = {
-					    make_shared<LightSingleGroup>(light_transform,
-					                                  rect_light)};
-
-					auto skysphere =
-					    make_shared<SkySphere>(make_shared<Constant>(Vec3(50)));
-					group.add(skysphere);
 					//  scene building end
 					auto sc = std::make_shared<Scene>(
 					    Scene(group, lights, obs, max_depth, sample_l));
@@ -136,7 +116,7 @@ int main() {
 					cout << "Start imaging to " << fnme << endl;
 					writeBmp(fnme.c_str(), img);
 					cout << "End" << endl;
-					std::cout << clkFinish - clkStart;
+					std::cout << clkFinish - clkStart << endl;
 				}
 			}
 		}
