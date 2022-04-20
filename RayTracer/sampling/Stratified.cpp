aa6618e0ec30298019d8bd4511df8863f8c80c64
Yoel <s73017@beuth-hochschule.de>
2022-04-20 12:53:28 +0200

    Finish the merge

--- RayTracer/sampling/Stratified.cpp
+++ RayTracer/sampling/Stratified.cpp
@@ -1,20 +1,29 @@
 #include "Stratified.h"
 
 namespace util {
-Stratified::Stratified(const std::shared_ptr<Sampler>& sampler, size_t n)
+Stratified::Stratified(const std::shared_ptr<OptiSampler>& sampler, size_t n)
     : sampler(sampler), n(n){};
 
-Vec3 Stratified::color(float x, float y) const {
+std::pair<Vec3, std::vector<int64_t>> Stratified::color_opti(float x,
+                                                             float y) const {
 	Vec3 color(0, 0, 0);
+	std::vector<int64_t> paths;
 	for (int xi = 0; xi < n; xi++) {
 		for (int yi = 0; yi < n; yi++) {
 			float rx = dis_0_to_1(gen);
 			float ry = dis_0_to_1(gen);
 			float xs = x + (xi + rx) / n;
 			float ys = y + (yi + ry) / n;
-			color = color + sampler->color(xs, ys);
+			auto color_pair = sampler->color_opti(xs, ys);
+			color = color + color_pair.first;
+			// Stats
+			auto prim = color_pair.second.at(0);
+			auto dire = color_pair.second.at(1);
+			paths.push_back(prim);
+			paths.push_back(dire);
+			paths.push_back(-1);
 		}
 	}
-	return color / (n * n);
+	return {color / (n * n), paths};
 }
 }  // namespace util
